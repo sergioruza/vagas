5f664ac2936c71eefe6d6adff94d5e06ab179bd3
Sergio <sergioruza22@gmail.com>
2023-06-12 09:49:40 -0300

    refactor: update user function to improve variable naming and error handling

--- teste4.js
+++ teste4.js
@@ -1,13 +1,21 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
 module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+    const { id } =  req.query;
+    const { name, job } = req.body;
+    const index = data.findIndex((user) => user.id == Number(id));
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    if (index === -1) {
+        return res.status(404).json({ message: 'Register not found.' });
+    };
 
-    res.send(reg);
+    const modifiedUser = {
+        id,
+        name,
+        job,
+    };
 
+    data[index] = modifiedUser;
+
+    return res.status(200).json(modifiedUser);
 };

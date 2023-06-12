0d92beb8ee1cc733557d0218db8918e2c1b30c73
Sergio <sergioruza22@gmail.com>
2023-06-12 14:33:36 -0300

    feat: Implementing token authentication function and error handling.

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = verifyToken;

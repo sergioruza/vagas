933b7e9477a6c27112bd728112b61291a0b19f13
Sergio <sergioruza22@gmail.com>
2023-06-12 11:48:36 -0300

    feat: implements logic for counter reads

--- teste1.js
+++ teste1.js
@@ -1,14 +1,19 @@
 const readFile = require('./utils/readFile');
 
 const data = readFile('./fakeData.json');
-
+const writeFile = require('./utils/writeFile');
 const getUser = (req, res) => {
     const { name } = req.query;
     const findByName = data.find((e) => e.name.toLowerCase() === name.toLowerCase());
-
+    
     if (!findByName) {
         return res.status(404).json({ message: 'Sorry, the requested record was not found.' });
     }
+
+    const indexByName = data.findIndex((user) => user.name == name);
+    !data[indexByName].counter ? data[indexByName].counter = 1 : data[indexByName].counter += 1;
+    writeFile('./fakeData.json', data);
+
     return res.status(200).json(findByName);
 };
 
@@ -16,6 +21,9 @@ const getUsers = (_req, res) => {
     if (data.length === 0) {
         return res.status(404).json({ message: 'No records found in the database.' });
     }
+
+    data.forEach((user) => !user.counter ? user.counter = 1 : user.counter += 1);
+    writeFile('./fakeData.json', data);
 
     return res.status(200).json(data);
 };

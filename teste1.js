6aba0079b9835f90418077bc81836dee91e8a729
Sergio <sergioruza22@gmail.com>
2023-06-12 08:26:39 -0300

    refactor: getUsers function to handle empty database and provide appropriate response

--- teste1.js
+++ teste1.js
@@ -12,10 +12,12 @@ const getUser = (req, res) => {
     return res.status(200).json(findByName);
 };
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
+const getUsers = (_req, res) => {
+    if (data.length === 0) {
+        return res.status(404).json({ message: 'No records found in the database.' });
+    }
+
+    return res.status(200).json(data);
 };
 
 module.exports = {

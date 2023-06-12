011cbb4d3a4f4aa5d22b868eaa6dc56b53723720
Sergio <sergioruza22@gmail.com>
2023-06-12 08:15:30 -0300

    Fix code indentation for better readability

--- teste1.js
+++ teste1.js
@@ -1,15 +1,15 @@
 const data = require("./fakeData");
 
-const getUser = ( req, res, next ) => {
-    const name =  req.query.name;
+const getUser = (req, res) => {
+    const name = req.query.name;
 
-    const findByName = data.find((e) => e.name === name)
+    const findByName = data.find((e) => e.name === name);
 
     if (!findByName) {
-        return res.status(404).json({ message: 'Sorry, the requested record was not found.' })
+        return res.status(404).json({ message: 'Sorry, the requested record was not found.' });
     }
     
-    return res.status(200).json(findByName)
+    return res.status(200).json(findByName);
 };
 
 const getUsers = ( req, res, next ) => {

7a7f06d93d50eaab9186d5cdd07cb1c56e195b68
Sergio <sergioruza22@gmail.com>
2023-06-12 14:49:58 -0300

    refactor: code separation in server.js file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-var teste1 = require("./teste1");
-var teste2 = require("./teste2");
-var teste3 = require("./teste3");
-var teste4 = require("./teste4");
-var teste5 = require("./teste5");
+const teste1 = require("./teste1");
+const teste2 = require("./teste2");
+const teste3 = require("./teste3");
+const teste4 = require("./teste4");
+const teste5 = require("./teste5");
 
 const verifyToken = require('./middlewares/verifyToken');
 
@@ -21,15 +21,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.send(`get user/ </br>
-  get users/ </br>
-  post users/ </br>
-  delete users/ </br>
-  put users/ </br>
-  `);
-});
-
 app.get("/user", teste1.getUser);
 app.get("/users", teste1.getUsers);
 app.post("/users", teste2.createUser)
@@ -37,8 +28,4 @@ app.delete("/users", verifyToken, teste3.deleteUser)
 app.put("/users", verifyToken, teste4.updateUserById)
 app.get("/users/access", teste5);
 
-
-const port  = 3000;
-app.listen(port, function(){
-  console.log('Express server listening on port ' + port);
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,17 @@
+
+const app = require('./app');
+
+app.get('/', function (req, res) {
+  res.send(`get user/ </br>
+  get users/ </br>
+  post users/ </br>
+  delete users/ </br>
+  put users/ </br>
+  `);
+});
+
+
+const port  = 3000;
+app.listen(port, function(){
+  console.log('Express server listening on port ' + port);
+});
